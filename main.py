76adbac167047b3592141391b3ba6382920271a5
aliferne <a3230269751@163.com>
2026-08-03 01:48:23 +0800

    feat. starting netease album parser, now resp2json ok

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from musicdl!")
+from musicdl.modules.sources import NeteaseMusicClient
 
+ncm = NeteaseMusicClient()
+album_url = "https://music.163.com/#/album?id=78989061"
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    ncm.parseAlbum(album_url)
+
